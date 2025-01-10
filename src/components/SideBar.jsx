d9847e0dd9b5f3c49110f1cd4b28272ee41132d3
ohmprakash-gloify <ohmprakash@gloify.com>
2025-01-10 10:11:20 +0530

    side bar control added

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,35 +1,78 @@
-import React from 'react'
+import React, { useState } from "react";
 
 // material UI
-import { Drawer, List, ListItem, ListItemText, Box } from '@mui/material'
+import {
+  Drawer,
+  List,
+  ListItem,
+  ListItemText,
+  Box,
+  IconButton,
+  AppBar,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
 
 //routes
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 const SideBar = () => {
+  const drawerWidth = 240;
+  const [open, setOpen] = useState(false);
 
-  const drawerWidth = 240
+  const handleDrawerToggle = () => {
+    setOpen((prevOpen) => !prevOpen);
+  };
   return (
-    <Box sx={{display: 'flex'}}>
-        <Drawer variant='permanent' sx={{
-        width: drawerWidth,
-        flexShrink: 0,
-        '& .MuiDrawer-paper': {
-          width: drawerWidth,
-          boxSizing: 'border-box',
-        },
-      }}>
-        <List>
-            <ListItem button component={Link} to="/users">
-                <ListItemText primary="Users" />
-            </ListItem>
-            <ListItem button component={Link} to="/cards">
-                <ListItemText primary="Cards" />
-            </ListItem>
-        </List>
-    </Drawer>
-    </Box>
-  )
-}
+    <Box sx={{ display: "flex" }}>
+      <AppBar
+        position="fixed"
+        sx={{
+          zIndex: (theme) => theme.zIndex.drawer + 1,
+        }}
+      >
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ marginRight: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap component="div">
+            Dashborad
+          </Typography>
+        </Toolbar>
+      </AppBar>
 
-export default SideBar
+      <Drawer
+        variant="permanent"
+        open={open}
+        sx={{
+            width: open ? drawerWidth : 0,
+            flexShrink: 0,
+            "& .MuiDrawer-paper": {
+              width: drawerWidth,
+              boxSizing: "border-box",
+              display: open ? "block" : "none",
+            },
+          }}
+      >
+        <Toolbar />
+        <List>
+          <ListItem button component={Link} to="/users">
+            <ListItemText primary="Users" />
+          </ListItem>
+          <ListItem button component={Link} to="/cards">
+            <ListItemText primary="Cards" />
+          </ListItem>
+        </List>
+      </Drawer>
+    </Box>
+  );
+};
+
+export default SideBar;
